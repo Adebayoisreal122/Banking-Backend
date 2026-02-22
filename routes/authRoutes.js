3f00418227d79293d1ced333db13d2b46b700872
adebayoisreal122 <adebayoisrealight122@gmail.com>
2026-02-22 14:05:43 +0100

    I rename route path to the auth route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,10 +4,10 @@ const authController = require('../controllers/authController');
 const { authenticateToken } = require('../middleware/auth');
 const { authLimiter } = require('../middleware/rateLimiter');
 
-router.post('/signup', authLimiter, authController.signup);
-router.post('/signin', authLimiter, authController.signin);
-router.get('/profile', authenticateToken, authController.getProfile);
-router.patch('/profile',         authenticate, exports.updateProfile);
-router.patch('/change-password', authenticate, exports.changePassword);
+router.post('/signup',  authLimiter, authController.signup);
+router.post('/signin',  authLimiter, authController.signin);
+router.get('/profile',  authenticateToken, authController.getProfile);
+router.patch('/profile',         authenticateToken, authController.updateProfile);   
+router.patch('/change-password', authenticateToken, authController.changePassword);  
 
 module.exports = router;
