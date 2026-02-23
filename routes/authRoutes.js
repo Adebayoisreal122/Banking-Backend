35fa6012537b5b6279289995e4c8804438d2f9e6
adebayoisreal122 <adebayoisrealight122@gmail.com>
2026-02-23 10:58:44 +0100

    I added upload image to cloudinary

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const { authenticateToken } = require('../middleware/auth');
 const { authLimiter } = require('../middleware/rateLimiter');
+const upload = require('../middleware/upload');
+
 
 router.post('/signup',  authLimiter, authController.signup);
 router.post('/signin',  authLimiter, authController.signin);
 router.get('/profile',  authenticateToken, authController.getProfile);
 router.patch('/profile',         authenticateToken, authController.updateProfile);   
-router.patch('/change-password', authenticateToken, authController.changePassword);  
+router.patch('/change-password', authenticateToken, authController.changePassword); 
+router.post('/avatar', authenticateToken, upload.single('avatar'), authController.uploadAvatar); 
 
 module.exports = router;
 
